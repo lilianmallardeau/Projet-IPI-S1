2f26af5a2d20566fdd68462ee6ed2fa17cceca20
Lilian Mallardeau <lilian.mallardeau@ensiie.fr>
2019-12-27 01:36:41 +0100

    Added comments in interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -5,8 +5,23 @@
 #include "matrix.h"
 #include "cursor.h"
 
+
+/* @requires: prog est une matrice contenant un programme p2d valide,
+ *   c'est à dire qui termine
+ * @assigns: nothing
+ * @ensures: execute les instructions du programme contenu dans la matrice prog
+ */
 void interpreter(matrix prog);
 
+
+/* Execute the next step of the p2d program
+ *
+ * @requires: *prog est une matrice contenant un programme p2d valide
+ *  0 <= cur->x <= prog.n, 0 <= cur->y <= prog.m
+ * @assigns: met à jour *prog, *cur et *prog_stack
+ * @ensures: execute l'instruction correspondant au caractère courant et
+ *  avance le curseur d'un pas dans la direction courante
+ */
 void one_step(matrix* prog, cursor* cur, stack* prog_stack);
 
 #endif
