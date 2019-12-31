aa2bd006531bdd6f031dfa3a20e47e2a4ad0e806
Lilian Mallardeau <lilian.mallardeau@ensiie.fr>
2019-12-31 16:42:19 +0100

    Added @requires, @assigns, @ensures for every function defined in debugger.c and debugger.h

--- debugger.c
+++ debugger.c
@@ -16,7 +16,7 @@
 
 /* @requieres: prog is a matrix of a valid p2d program
  * @assigns:
- * @ensures:
+ * @ensures: runs the p2d debugger on the p2d program in the matrix prog_mat
  */
 void debugger(matrix prog_mat) {
   stack prog_stack;
@@ -122,6 +122,10 @@ void debugger(matrix prog_mat) {
 /* ---------------------------- Breakpoints functions ---------------------------- */
 /* ------------------------------------------------------------------------------- */
 
+/* @requieres: *breakpoints is of size *buffer_size, 0 <= *n_breakpoints <= *buffer_size
+ * @assigns: updates *n_breakpoints, reallocates *breakpoints if needed and updates *buffer_size
+ * @ensures: adds the breakpoint (x, y) to the breakpoints array if it doesn't already exist, and reallocates the array if needed
+ */
 int add_breakpoint(int x, int y, breakpoint* breakpoints[], size_t* n_breakpoints, size_t* buffer_size) {
   if (!check_breakpoint(x, y, *breakpoints, *n_breakpoints)) {
     breakpoint new_breakpoint;
@@ -147,26 +151,40 @@ int add_breakpoint(int x, int y, breakpoint* breakpoints[], size_t* n_breakpoint
   }
 }
 
-void remove_breakpoint(int x, int y, breakpoint breakpoints[], size_t* len) {
-  if (check_breakpoint(x, y, breakpoints, *len)) {
-    int index = get_breakpoint(x, y, breakpoints, *len);
-    for (int i = index; i < *len - 1; i++) {
+/* @requieres: breakpoints is of size >= *n_breakpoints
+ * @assigns: removes the breakpoint (x, y) from the breakpoints array if present, shifts all
+ *    the next breakpoints, and updates *n_breakpoints
+ * @ensures: removes the breakpoint (x, y) from the breakpoints array if present, and updates *n_breakpoints
+ */
+void remove_breakpoint(int x, int y, breakpoint breakpoints[], size_t* n_breakpoints) {
+  if (check_breakpoint(x, y, breakpoints, *n_breakpoints)) {
+    int index = get_breakpoint(x, y, breakpoints, *n_breakpoints);
+    for (int i = index; i < *n_breakpoints - 1; i++) {
       breakpoints[i] = breakpoints[i+1];
     }
-    (*len)--;
+    (*n_breakpoints)--;
   }
 }
 
-int get_breakpoint(int x, int y, breakpoint breakpoints[], size_t len) {
-  for (int i = 0; i < len; i++) {
+/* @requieres: breakpoints is of size >= n_breakpoints
+ * @assigns: nothing
+ * @ensures: returns the index of the breakpoint (x, y) in the breakpoints array, or -1 if
+ *    it doesn't exist or isn't in the n_breakpoints first elements of the breakpoints array
+ */
+int get_breakpoint(int x, int y, breakpoint breakpoints[], size_t n_breakpoints) {
+  for (int i = 0; i < n_breakpoints; i++) {
     if (breakpoints[i].x == x && breakpoints[i].y == y)
       return i;
   }
   return -1;
 }
 
-int check_breakpoint(int x, int y, breakpoint breakpoints[], size_t len) {
-  return get_breakpoint(x, y, breakpoints, len) != -1;
+/* @requieres: breakpoints is of size >= n_breakpoints
+ * @assigns: nothing
+ * @ensures: returns 1 if the breakpoint (x, y) exists in the array breakpoints, 0 otherwise
+ */
+int check_breakpoint(int x, int y, breakpoint breakpoints[], size_t n_breakpoints) {
+  return get_breakpoint(x, y, breakpoints, n_breakpoints) != -1;
 }
 
 
@@ -174,10 +192,12 @@ int check_breakpoint(int x, int y, breakpoint breakpoints[], size_t len) {
 /* ------------------------------ History functions ------------------------------ */
 /* ------------------------------------------------------------------------------- */
 
-/* @requieres: prog is a matrix of a valid p2d program, cur is a valid cursor,
- *    prog_stack is a valid stack, , 0 <= index
- * @assigns: Reallocate *saved_steps array if needed.
- * @ensures:
+/* @requieres: prog is a valid matrix of a valid p2d program, cur is a valid cursor,
+ *    prog_stack is a valid stack, 0 <= index, *saved_steps has a size of *size
+ * @assigns: Reallocates *saved_steps array if needed (and updates *size), adds the newly created
+ *    prog_step in the saved_steps array
+ * @ensures: saves the current state of the program in the *saved_steps array (and reallocates the
+ *    array and updates *size if needed)
  */
 void save_step(int index, matrix mat, cursor cur, stack prog_stack, prog_step* saved_steps[], size_t* size) {
   prog_step new_step;
@@ -199,9 +219,9 @@ void save_step(int index, matrix mat, cursor cur, stack prog_stack, prog_step* s
   (*saved_steps)[index] = new_step;
 }
 
-/* @requieres:
- * @assigns:
- * @ensures:
+/* @requieres: saved_steps is of size > index, saved_steps[index] is a valid prog_step element
+ * @assigns: updates *mat, *cur, *prog_stack with the saved ones in saved_steps[index]
+ * @ensures: restores the index-th saved step
  */
 void restore(int index, matrix* mat, cursor* cur, stack* prog_stack, prog_step saved_steps[]) {
   *mat = copy_matrix(saved_steps[index].mat);
@@ -209,9 +229,9 @@ void restore(int index, matrix* mat, cursor* cur, stack* prog_stack, prog_step s
   *prog_stack = copy_stack(saved_steps[index].stack);
 }
 
-/* @requieres:
- * @assigns:
- * @ensures:
+/* @requieres: *prog_mat is a valid p2d program matrix, *cur a valid cursor, *prog_stack a valid stack, *index >= 0
+ * @assigns: updates *prog_mat, *cur, *prog_stack, *index and saves the new state of the program in *saved_steps (and realloc it and updates *size if needed)
+ * @ensures: executes one step in the program, saves the new state of the program in *saved_steps, and updates *index
  */
 void next(int* index, matrix* prog_mat, cursor* cur, stack* prog_stack, prog_step* saved_steps[], size_t* size) {
   one_step(prog_mat, cur, prog_stack);
@@ -219,9 +239,9 @@ void next(int* index, matrix* prog_mat, cursor* cur, stack* prog_stack, prog_ste
   (*index)++;
 }
 
-/* @requieres:
- * @assigns:
- * @ensures:
+/* @requieres: saved_steps is of size > index - 1, saved_steps[min(index-1, 0)] is a valid prog_step element
+ * @assigns: updates *prog_mat, *cur, *prog_stack with the ones of the previous step of the program
+ * @ensures: restores the (index-1)-th step of the program if index >= 0, otherwise restores initial state of the program (before execution)
  */
 void prev(int* index, matrix* prog_mat, cursor* cur, stack* prog_stack, prog_step saved_steps[]) {
   if (*index - 1 >= 0) {
@@ -249,7 +269,8 @@ void clear_screen() {
 
 /* @requieres: prog is a valid matrix, cur a valid cursor, and prog_stack a valid stack
  * @assigns: nothing
- * @ensures: clears the console and prints the current state of execution of the program
+ * @ensures: clears the console and prints the current state of execution of the program,
+ *    the program stack and the cursor
  */
 void print_screen(matrix prog, cursor cur, stack prog_stack) {
   printf("\n");
@@ -261,7 +282,7 @@ void print_screen(matrix prog, cursor cur, stack prog_stack) {
   printf("\n    ");
   for (int i = 0; i < prog.n + 5; i++) {
     if (i == cur.x)
-      printf(BOLDYELLOW "v" RESET);
+      printf(BOLDRED "v" RESET);
     else
       printf("%c", i%5 == 0 ? '|' : ' ');
   }
@@ -270,12 +291,12 @@ void print_screen(matrix prog, cursor cur, stack prog_stack) {
   // Printing program matrix
   for (int i = 0; i < prog.m; i++) {
     if (i%5 == 0)
-      printf("%3d%s", i, cur.y == i ? BOLDYELLOW ">" RESET : "-");
+      printf("%3d%s", i, cur.y == i ? BOLDRED ">" RESET : "-");
     else
-      printf("%3c%s", ' ', cur.y == i ? BOLDYELLOW ">" RESET : " ");
+      printf("%3c%s", ' ', cur.y == i ? BOLDRED ">" RESET : " ");
 
     for (int j = 0; j < prog.n; j++)
-      printf((cur.x == j && cur.y == i) ? BOLDRED "%c" RESET : "%c", prog.mat[i][j]);
+      printf((cur.x == j && cur.y == i) ? INVERTED "%c" RESET : "%c", prog.mat[i][j]);
     printf("\n");
   }
   printf("\n");
--- debugger.h
+++ debugger.h
@@ -6,7 +6,6 @@
 #include "cursor.h"
 
 /* ------------------------------------ Types ------------------------------------ */
-
 /* A step in the program is saved with the program's matrix, the current cursor and the current stack */
 typedef struct prog_step prog_step;
 struct prog_step {
@@ -21,23 +20,84 @@ struct breakpoint {
   int x, y;
 };
 
+
 /* ---------------------------------- Debugger ----------------------------------- */
+/* @requieres: prog is a matrix of a valid p2d program
+ * @assigns:
+ * @ensures: runs the p2d debugger on the p2d program in the matrix prog_mat
+ */
 void debugger(matrix prog_mat);
 
+
 /* ---------------------------- Breakpoints functions ---------------------------- */
+/* @requieres: *breakpoints is of size *buffer_size, 0 <= *n_breakpoints <= *buffer_size
+ * @assigns: updates *n_breakpoints, reallocates *breakpoints if needed and updates *buffer_size
+ * @ensures: adds the breakpoint (x, y) to the breakpoints array if it doesn't already exist, and reallocates the array if needed
+ */
 int add_breakpoint(int x, int y, breakpoint* breakpoints[], size_t* n_breakpoints, size_t* buffer_size);
-void remove_breakpoint(int x, int y, breakpoint breakpoints[], size_t* len);
-int get_breakpoint(int x, int y, breakpoint breakpoints[], size_t len);
-int check_breakpoint(int x, int y, breakpoint breakpoints[], size_t len);
+
+/* @requieres: breakpoints is of size >= *n_breakpoints
+ * @assigns: removes the breakpoint (x, y) from the breakpoints array if present, shifts all
+ *    the next breakpoints, and updates *n_breakpoints
+ * @ensures: removes the breakpoint (x, y) from the breakpoints array if present, and updates *n_breakpoints
+ */
+void remove_breakpoint(int x, int y, breakpoint breakpoints[], size_t* n_breakpoints);
+
+/* @requieres: breakpoints is of size >= n_breakpoints
+ * @assigns: nothing
+ * @ensures: returns the index of the breakpoint (x, y) in the breakpoints array, or -1 if
+ *    it doesn't exist or isn't in the n_breakpoints first elements of the breakpoints array
+ */
+int get_breakpoint(int x, int y, breakpoint breakpoints[], size_t n_breakpoints);
+
+/* @requieres: breakpoints is of size >= n_breakpoints
+ * @assigns: nothing
+ * @ensures: returns 1 if the breakpoint (x, y) exists in the array breakpoints, 0 otherwise
+ */
+int check_breakpoint(int x, int y, breakpoint breakpoints[], size_t n_breakpoints);
+
 
 /* ------------------------------ History functions ------------------------------ */
+/* @requieres: prog is a valid matrix of a valid p2d program, cur is a valid cursor,
+ *    prog_stack is a valid stack, 0 <= index, *saved_steps has a size of *size
+ * @assigns: Reallocates *saved_steps array if needed (and updates *size), adds the newly created
+ *    prog_step in the saved_steps array
+ * @ensures: saves the current state of the program in the *saved_steps array (and reallocates the
+ *    array and updates *size if needed)
+ */
 void save_step(int index, matrix mat, cursor cur, stack prog_stack, prog_step* saved_steps[], size_t* size);
+
+/* @requieres: saved_steps is of size > index, saved_steps[index] is a valid prog_step element
+ * @assigns: updates *mat, *cur, *prog_stack with the saved ones in saved_steps[index]
+ * @ensures: restores the index-th saved step
+ */
 void restore(int index, matrix* mat, cursor* cur, stack* prog_stack, prog_step saved_steps[]);
+
+/* @requieres: *prog_mat is a valid p2d program matrix, *cur a valid cursor, *prog_stack a valid stack, *index >= 0
+ * @assigns: updates *prog_mat, *cur, *prog_stack, *index and saves the new state of the program in *saved_steps (and realloc it and updates *size if needed)
+ * @ensures: executes one step in the program, saves the new state of the program in *saved_steps, and updates *index
+ */
 void next(int* index, matrix* prog_mat, cursor* cur, stack* prog_stack, prog_step* saved_steps[], size_t* size);
+
+/* @requieres: saved_steps is of size > index - 1, saved_steps[min(index-1, 0)] is a valid prog_step element
+ * @assigns: updates *prog_mat, *cur, *prog_stack with the ones of the previous step of the program
+ * @ensures: restores the (index-1)-th step of the program if index >= 0, otherwise restores initial state of the program (before execution)
+ */
 void prev(int* index, matrix* prog_mat, cursor* cur, stack* prog_stack, prog_step saved_steps[]);
 
+
 /* ------------------------------ Screens functions ------------------------------ */
+/* @requieres: nothing
+ * @assigns: nothing
+ * @ensures: Clears the console. Works on Linux (bash/zsh), Windows (cmd.exe) and macOS (zsh)
+ */
 void clear_screen();
+
+/* @requieres: prog is a valid matrix, cur a valid cursor, and prog_stack a valid stack
+ * @assigns: nothing
+ * @ensures: clears the console and prints the current state of execution of the program,
+ *    the program stack and the cursor
+ */
 void print_screen(matrix prog, cursor cur, stack prog_stack);
 
 
